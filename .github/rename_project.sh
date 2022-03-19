d5d6d1c2e019352c18bec851ae3e7072b76ac433
timothyleong97 <timothyleong97@users.noreply.github.com>
2022-03-19 12:24:56 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="cs4221"
+original_name="xpath"
+original_urlname="xpath"
+original_description="Awesome xpath created by cs4221"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv xpath $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- xpath/base.py
+++ xpath/base.py
@@ -1,7 +1,7 @@
 """
-project_name base module.
+xpath base module.
 
-This is the principal module of the project_name project.
+This is the principal module of the xpath project.
 here you put your main classes and objects.
 
 Be creative! do whatever you want!
