a4485b49d6d3b84eafc65965139895ba44db6059
LeeEnHao <69501134+LeeEnHao@users.noreply.github.com>
2022-03-26 05:53:34 +0800

    update json test

--- tests/test_jsonconverter.py
+++ tests/test_jsonconverter.py
@@ -17,3 +17,23 @@ def test_json_to_tree_raw_value():
     with open("output.xml", "r") as file:
         data = file.read().rstrip()
         assert data == "<root>1</root>"
+
+        
+def test_json_to_tree_array_string():
+    test_str = ('{"employees":['
+                '{ "firstName":"John", "lastName":"Doe" },'
+                '{ "firstName":"Anna", "lastName":"Smith" },'
+                '{ "firstName":"Peter", "lastName":"Jones" }'
+                ']}')
+    expected_str = ("<root><employees>"
+                    "<element><firstName>John</firstName>"
+                    "<lastName>Doe</lastName></element><element>"
+                    "<firstName>Anna</firstName><lastName>Smith</lastName>"
+                    "</element><element><firstName>Peter</firstName><lastName>"
+                    "Jones</lastName></element>"
+                    "</employees></root>")
+    tree = json_to_tree(test_str)
+    tree.write("output.xml")
+    with open("output.xml", "r") as file:
+        data = file.read().strip()
+        assert data == expected_str
