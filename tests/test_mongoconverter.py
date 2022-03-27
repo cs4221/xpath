e92b6a614907c81e984c80eb7e3382c92047cabd
Timothy Leong <timothy.leong@u.nus.edu>
2022-03-27 15:13:15 +0800

    chore: add log

--- tests/test_mongoconverter.py
+++ tests/test_mongoconverter.py
@@ -3,6 +3,4 @@ from pymongo import MongoClient
 
 def test_connection():
     client = MongoClient("localhost", 27017)
-    client.server_info()  # force connection on a request as the
-    # connect=True parameter of MongoClient seems
-    # to be useless here
+    print(client.server_info())  # force connection
