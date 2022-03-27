3c5498b858ad7b19b39f219eeb4e7f1efe71d556
Timothy Leong <timothy.leong@u.nus.edu>
2022-03-27 10:45:53 +0800

    test: connecting to mongodb from all oses

---
+++ tests/test_mongoconverter.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+
+# Check if all OSes can access the same MongoDB instance
+def test_connection():
+    client = MongoClient(
+        "mongodb://localhost:27017/", serverSelectionTimeoutMS=0
+    )
+    client.server_info()  # force connection on a request as the
+    # connect=True parameter of MongoClient seems
+    # to be useless here
