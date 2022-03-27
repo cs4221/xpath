34d9ff8b657d15b3aa6345cc7aaa86ba45b4ba82
Timothy Leong <timothy.leong@u.nus.edu>
2022-03-27 16:07:52 +0800

    test: connect pytest to mongo

--- tests/test_mongoconverter.py
+++ tests/test_mongoconverter.py
@@ -4,3 +4,9 @@ from pymongo import MongoClient
 def test_connection():
     client = MongoClient("localhost", 27017)
     print(client.server_info())  # force connection
+
+
+def test_players(mongodb):
+    assert "players" in mongodb.list_collection_names()
+    manuel = mongodb.players.find_one({"name": "Manuel"})
+    assert manuel["surname"] == "Neuer"
