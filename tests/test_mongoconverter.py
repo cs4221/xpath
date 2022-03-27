cb0bb56b85ec2b5077f962ab6b76acd4f9121f3a
Timothy Leong <timothy.leong@u.nus.edu>
2022-03-27 16:20:48 +0800

    test: remove pytest-mongodb

--- tests/test_mongoconverter.py
+++ tests/test_mongoconverter.py
@@ -1,5 +1,6 @@
-# Check that data is read in
-def test_players(mongodb):
-    assert "players" in mongodb.list_collection_names()
-    manuel = mongodb.players.find_one({"name": "Manuel"})
-    assert manuel["surname"] == "Neuer"
+from pymongo import MongoClient
+
+
+def test_connection():
+    client = MongoClient("localhost", 27017)
+    print(client.server_info())  # force connection
