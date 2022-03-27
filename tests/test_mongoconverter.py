5b0189c5cb9b081a6c571bc4d89f918b6547b2b3
Timothy Leong <timothy.leong@u.nus.edu>
2022-03-27 16:15:27 +0800

    chore: remove old mongo test code

--- tests/test_mongoconverter.py
+++ tests/test_mongoconverter.py
@@ -1,11 +1,4 @@
-from pymongo import MongoClient
-
-
-def test_connection():
-    client = MongoClient("localhost", 27017)
-    print(client.server_info())  # force connection
-
-
+# Check that data is read in
 def test_players(mongodb):
     assert "players" in mongodb.list_collection_names()
     manuel = mongodb.players.find_one({"name": "Manuel"})
