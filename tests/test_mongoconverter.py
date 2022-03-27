93b4510c456f246b86514ca292abb0203ff4f3f8
Timothy Leong <timothy.leong@u.nus.edu>
2022-03-27 17:24:52 +0800

    feat: add ability to load all jsons

--- tests/test_mongoconverter.py
+++ tests/test_mongoconverter.py
@@ -15,13 +15,15 @@ def mongo_client():
     # If database does not exist, create it
     if not database_name in client.list_database_names():
         database = client[database_name]
-
+        # For each json file...
         for filename in os.listdir(data_directory):
+            # Extract the json name, e.g. players.json -> players.
             collection_name = os.path.splitext(os.path.split(filename)[1])[0]
             f = os.path.join(data_directory, filename)
 
             with open(f, "r") as file:
                 data = json.loads(file.read().strip())
+                # Set the collection name to be that of the json file.
                 collection = database[collection_name]
                 if isinstance(data, list):
                     collection.insert_many(data)
@@ -32,8 +34,16 @@ def mongo_client():
     client.close()
 
 
+# Test for successful loading of json
 def test_players(mongo_client):
     database = mongo_client[database_name]
     assert "players" in database.list_collection_names()
     manuel = database.players.find_one({"name": "Manuel"})
     assert manuel["surname"] == "Neuer"
+
+
+def test_inventory(mongo_client):
+    database = mongo_client[database_name]
+    assert "inventory" in database.list_collection_names()
+    paper = database.inventory.find_one({"item": "paper"})
+    assert paper["qty"] == 100
