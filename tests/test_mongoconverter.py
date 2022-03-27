3ca79580d205e529c420e61936828128f51335e9
Timothy Leong <timothy.leong@u.nus.edu>
2022-03-27 15:07:57 +0800

    fix: initialise MongoClient

--- tests/test_mongoconverter.py
+++ tests/test_mongoconverter.py
@@ -2,9 +2,7 @@ from pymongo import MongoClient
 
 
 def test_connection():
-    client = MongoClient(
-        "mongodb://localhost:27017/", serverSelectionTimeoutMS=0
-    )
+    client = MongoClient("localhost", 27017)
     client.server_info()  # force connection on a request as the
     # connect=True parameter of MongoClient seems
     # to be useless here
