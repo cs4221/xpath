f842cbf5fc7c58c63b79af9937935392fe491844
Timothy Leong <timothy.leong@u.nus.edu>
2022-03-28 15:02:04 +0800

    chore: remove deadcode from template

--- xpath/__init__.py
+++ xpath/__init__.py
@@ -1,11 +1,8 @@
-from .base import BaseClass, base_function
 from .elemconverter import elem_to_tree, insert
 from .jsonconverter import json_to_tree
 from .mongoconverter import PyMongoElement
 
 __all__ = [
-    "BaseClass",
-    "base_function",
     "insert",
     "elem_to_tree",
     "json_to_tree",
