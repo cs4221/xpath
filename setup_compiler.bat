b37df74365c9551c16aea73dc4a0f50d75e22987
KenGondor <kengondorleh@gmail.com>
2022-04-01 12:46:38 +0800

    update: batch file to generate and move files to compiler folder

--- setup_compiler.bat
+++ setup_compiler.bat
@@ -1,1 +1,4 @@
 java org.antlr.v4.Tool -Dlanguage=Python3 -o ./xpath/xpathgrammer/.antlr4py3 %*
+
+del /S /Q .\xpath\compiler\X*
+move /y .\xpath\xpathgrammer\.antlr4py3\xpath\xpathgrammer\* .\xpath\compiler\
