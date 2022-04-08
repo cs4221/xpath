b51f31a4c44e4c0af925231f96041497873423ca
KenGondor <kengondorleh@gmail.com>
2022-04-08 22:20:03 +0800

    update batch file for setup parser

--- setup_compiler.bat
+++ setup_compiler.bat
@@ -1,4 +1,4 @@
 java org.antlr.v4.Tool -Dlanguage=Python3 -o ./xpath/xpathgrammer/.antlr4py3 %*
 
-del /S /Q .\xpath\compiler\X*
-move /y .\xpath\xpathgrammer\.antlr4py3\xpath\xpathgrammer\* .\xpath\compiler\
+del /S /Q .\xpath\compilerv1\X*
+move /y .\xpath\xpathgrammer\.antlr4py3\xpath\xpathgrammer\* .\xpath\compilerv1\
